50efd813ce5a4b7557a68a51e9d32be3e8e8a146
tarik <bayzin@hotmail.com.tr>
2020-11-13 17:26:12 +0300

    Initial commit

---
+++ src/main/java/com/example/apiConnection/ApiConnectionApplication.java
@@ -0,0 +1,13 @@
+package com.example.apiConnection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiConnectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiConnectionApplication.class, args);
+	}
+
+}
