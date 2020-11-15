f285a43bd36227a67370a67a0ab6119d27b9a817
tbayzin <bayzin@hotmail.com.tr>
2020-11-15 22:46:54 +0300

    getmapping çalışıyor,dönüyor

---
+++ src/main/java/com/example/apiConnection/Connector.java
@@ -0,0 +1,10 @@
+package com.example.apiConnection;
+
+import com.example.apiConnection.controller.GetController;
+
+public class Connector {
+
+ 
+
+
+}
--- src/main/java/com/example/apiConnection/controller/GetController.java
+++ src/main/java/com/example/apiConnection/controller/GetController.java
@@ -1,4 +1,4 @@
-package com.example.apiConnection;
+package com.example.apiConnection.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
