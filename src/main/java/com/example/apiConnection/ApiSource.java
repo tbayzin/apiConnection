8c1b78ebd247286d69214c0bdd7e7f84e2b74796
tbayzin <bayzin@hotmail.com.tr>
2020-11-15 11:46:22 +0300

    getmapping çalışıyor,dönüyor

--- src/main/java/com/example/apiConnection/ApiSource.java
+++
@@ -1,9 +0,0 @@
-package com.example.apiConnection;
-
-import org.springframework.stereotype.Controller;
-
-@Controller
-public class ApiSource {
-
-
-}
---
+++ src/main/java/com/example/apiConnection/GetController.java
@@ -0,0 +1,36 @@
+package com.example.apiConnection;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Random;
+
+@Controller
+@RequestMapping ("/ozgurproject")
+public class GetController {
+
+      boolean isEven;
+
+         Random number = new Random();
+        int number1 = number.nextInt(10);
+
+      @RequestMapping(value = "/api", method = RequestMethod.GET)
+@ResponseBody
+    public boolean getResult () {
+
+        if (number1 %2 ==0 ) {
+            isEven = true;
+
+        }
+
+        else {
+            isEven = false;
+        }
+
+        return isEven;
+    }
+
+}
